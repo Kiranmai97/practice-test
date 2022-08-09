a1e4966322a25a604e0cfd85a98ef8a04565d67d
Kiranmai0415 <ubbanirubenkiranmai@gmail.com>
2022-08-09 09:51:57 +0530

    mounting,updating and unmounting

--- src/components/updating.js
+++ src/components/updating.js
@@ -1,10 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 function Hooks(){
 
+    const [count, setCount] = useState(0);
+    // Mounting 
+useEffect(()=>{
+    console.log(" Mount rendering... ")
+})
+
+// updating
+useEffect(()=>{
+ console.log("it will render multiple times")
+//  setCount(count+1)
+},[count])
+
+ useEffect(() => {
+        // Anything in here is fired on component mount.
+        return () => {
+            // Anything in here is fired on component unmount.
+        }
+    }, [])
+
+
+
+
+
     return(
         <div>
-            Hii  Updating , Mounting and UnMounting
+       <button onClick={() => setCount(count)}>
+        Click me
+      </button>
         </div>
     );
 }
