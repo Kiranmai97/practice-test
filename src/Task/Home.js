c113a34813f1b278857bdf94fa704b8d570d2a43
Kiranmai97 <106075037+Kiranmai97@users.noreply.github.com>
2022-08-26 18:17:40 +0530

    changes

--- src/Task/Home.js
+++ src/Task/Home.js
@@ -5,25 +5,15 @@ const Home = () => {
     return (
         <div>
             <h4 className='h4'>Project Management</h4>
-
             <div className='homePage'>
-                {/* <div className='project'>
-                <span className='center'>PRE</span>
-                </div>
-             */}
-
                 <div className='grid' >
-                 
-                
-                        <div><a href="/pre"><img src={Test} className="project"></img></a></div>
-                        <div> <a href="/pro"> <img src={Test} className="project"></img></a></div>
-                        <div> <a href="/test"> <img src={Test} className="project"></img></a></div>
-                        <div> <a href="/add"> <img src={Test} className="project"></img></a></div>
-                    
+                    <div><h6>Pre</h6><a href="/pre"><img src={Test} className="project"></img></a></div>
+                    <div><h6>Pro</h6> <a href="/pro"> <img src={Test} className="project"></img></a></div>
+                    <div><h6>Test</h6><a href="/test"> <img src={Test} className="project"></img></a></div>
+                    <div><h6>Add</h6><a href="/add"> <img src={Test} className="project"></img></a></div>
                 </div>
             </div>
         </div>
     )
 }
-
 export default Home
